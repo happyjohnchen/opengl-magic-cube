d631a59ce9914f3e14f5817f48dca7727194c34f
johnchen <johnchen@DESKTOP-CUOFTC9>
2021-10-31 22:37:03 +0800

    添加项目文件。

---
+++ OpenGLMagicCube/OpenGLMagicCube.cpp
@@ -0,0 +1,149 @@
+﻿// OpenGLMagicCube.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <algorithm>
+#include<math.h>
+#include <GL/glut.h>
+
+using namespace std;
+
+bool mouseLeftDown = false;
+bool mouseRightDown = false;
+
+float xrot = 0.0f;
+float yrot = 0.0f;
+
+float xdiff = 0.0f;
+float ydiff = 0.0f;
+
+// 绘制正方体
+void drawBox() {
+    glBegin(GL_QUADS);
+
+    // 前面
+    glColor3f(0.2f, 0.2f, 0.8f);
+    glVertex3f(-0.5f, -0.5f, 0.5f);
+    glVertex3f(0.5f, -0.5f, 0.5f);
+    glVertex3f(0.5f, 0.5f, 0.5f);
+    glVertex3f(-0.5f, 0.5f, 0.5f);
+
+    // 后面
+    glColor3f(0.2f, 0.8f, 0.2f);
+    glVertex3f(-0.5f, -0.5f, -0.5f);
+    glVertex3f(-0.5f, 0.5f, -0.5f);
+    glVertex3f(0.5f, 0.5f, -0.5f);
+    glVertex3f(0.5f, -0.5f, -0.5f);
+
+    // 左面
+    glColor3f(0.8f, 0.5f, 0.2f);
+    glVertex3f(-0.5f, -0.5f, 0.5f);
+    glVertex3f(-0.5f, 0.5f, 0.5f);
+    glVertex3f(-0.5f, 0.5f, -0.5f);
+    glVertex3f(-0.5f, -0.5f, -0.5f);
+
+    // 右面
+    glColor3f(0.8f, 0.2f, 0.2f);
+    glVertex3f(0.5f, -0.5f, -0.5f);
+    glVertex3f(0.5f, 0.5f, -0.5f);
+    glVertex3f(0.5f, 0.5f, 0.5f);
+    glVertex3f(0.5f, -0.5f, 0.5f);
+
+    // 顶面
+    glColor3f(0.8f, 0.8f, 0.2f);
+    glVertex3f(-0.5f, 0.5f, 0.5f);
+    glVertex3f(0.5f, 0.5f, 0.5f);
+    glVertex3f(0.5f, 0.5f, -0.5f);
+    glVertex3f(-0.5f, 0.5f, -0.5f);
+
+    // 底面
+    glColor3f(1.0f, 1.0f, 1.0f);
+    glVertex3f(-0.5f, -0.5f, 0.5f);
+    glVertex3f(-0.5f, -0.5f, -0.5f);
+    glVertex3f(0.5f, -0.5f, -0.5f);
+    glVertex3f(0.5f, -0.5f, 0.5f);
+    glEnd();
+}
+
+// 绘制函数
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
+
+    gluLookAt(
+        0.0f, 0.0f, 3.0f,
+        0.0f, 0.0f, 0.0f,
+        0.0f, 1.0f, 0.0f);
+
+    // 实现鼠标旋转的核心
+    glRotatef(xrot, 1.0f, 0.0f, 0.0f);
+    glRotatef(yrot, 0.0f, 1.0f, 0.0f);
+
+    drawBox();
+
+    glFlush();
+    glutSwapBuffers();
+}
+
+void resize(int w, int h) {
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+
+    glViewport(0, 0, w, h);
+
+    gluPerspective(45.0f, 1.0f * w / h, 1.0f, 100.0f);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+}
+
+// 鼠标事件
+void mouse(int button, int state, int x, int y) {
+    if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN) {
+        mouseRightDown = true;
+        xdiff = x - yrot;
+        ydiff = -y + xrot;
+        cout << "xdiff:" << xdiff << "\tydiff" << ydiff << endl;
+    }
+    else
+        mouseRightDown = false;
+}
+
+// 鼠标移动事件
+void mouseMotion(int x, int y) {
+    if (mouseRightDown) {
+        yrot = x - xdiff;
+        xrot = y + ydiff;
+        cout << "yrot:" << yrot << "\txrot" << xrot << endl;
+
+        glutPostRedisplay();
+    }
+}
+
+int main(int argc, char* argv[]) {
+    glutInit(&argc, argv);
+
+    glutInitWindowPosition(400, 100);
+    glutInitWindowSize(800, 800);
+
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+
+    glutCreateWindow("Magic Cube");
+
+    glutDisplayFunc(display);
+    glutMouseFunc(mouse);
+    glutMotionFunc(mouseMotion);
+    glutReshapeFunc(resize);
+
+    glClearColor(0.93f, 0.93f, 0.93f, 0.0f);
+
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LEQUAL);
+    glClearDepth(1.0f);
+
+    glutMainLoop();
+    return 0;
+}
+
+
